d8302be665ab052e590e2d0f2176abca01547bbd
Jorge Bautista <jorgebautista@gmail.com>
2021-03-09 17:26:13 -0500

    added routes to router

---
+++ app/url_mappings.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"gitlab.com/jebo87/makako-gateway/controllers/listings"
+)
+
+// 	// router.HandleFunc("/ads", httputils.ValidateMiddleware(adsHandler)).Methods("GET", "OPTIONS")
+// 	// router.HandleFunc("/ads", (gwhandlers.AdsHandler)).Methods("GET", "OPTIONS")
+// 	router.HandleFunc("/ads", (gwhandlers.ListingHandler)).Methods("GET", "POST", "OPTIONS")
+// 	router.HandleFunc("/ads/{key}", gwhandlers.AdHandler).Methods("GET", "OPTIONS")
+// 	router.HandleFunc("/listing/new", httputils.ValidateMiddleware(gwhandlers.NewListingHandler)).Methods("POST", "OPTIONS")
+// 	router.HandleFunc("/ad_count", httputils.ValidateMiddleware(gwhandlers.AdsCountHandler)).Methods("GET", "OPTIONS")
+// 	router.HandleFunc("/{userId}/listings", httputils.ValidateMiddleware(gwhandlers.UserListingsHandler)).Methods("POST", "OPTIONS")
+
+func MapURLs() {
+	router.GET("/listings" /*middleware.ValidateMiddleware(),*/, listings.GetListings)
+}
